bf8e90cbea2c040ad5d39074c3dd338756b397bd
ericw <ericw@UncleRicco.san.rr.com>
2025-01-29 18:09:31 -0800

    Print function to learn.

--- src/demo1/DemoTest2.java
+++ src/demo1/DemoTest2.java
@@ -3,5 +3,7 @@ package demo1;
 public class DemoTest2 {
 	public static void main(String[] args) {
 		System.out.println("Java");
+		
+		System.out.println("There is a lot of material to learn here");
 }
 }
