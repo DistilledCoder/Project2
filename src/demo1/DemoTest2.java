f482df0efc2f73d6437139aec34c83be587cdfba
Uncle Ricco <eric4cali@gmail.com>
2025-01-29 18:28:52 -0800

    Update DemoTest2.java
    Test

--- src/demo1/DemoTest2.java
+++ src/demo1/DemoTest2.java
@@ -5,5 +5,17 @@ public class DemoTest2 {
 		System.out.println("Java");
 		
 		System.out.println("There is a lot of material to learn here");
+
+		int a = 10;
+		int b = 20;
+		
+		if (a > b) {
+			System.out.println("Greater Than");
+		} else {
+			System.out.println("Less Than");
+			
+		}
+
+		
 }
 }
